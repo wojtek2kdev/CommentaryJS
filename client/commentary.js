df25f7ed610b42da29a87c56b9c0d84b37b35d99
wojtek2kdev <wojtek2kdev@gmail.com>
2019-02-15 20:23:30 +0100

    Update commentary.js   - Add default css classes to constructor   - Create fetch comments function   - Create comment generator

--- client/commentary.js
+++ client/commentary.js
@@ -1,20 +1,58 @@
-class Commentary {
+class Channel {
 
 	constructor({
 		target="#commentary",
-		channelsURI,
+		wsURI,
+		commentsURI,
 	} = {},
 	{
-		commentClass="comment",
+		commentListClass="comments",
+		commentContainerClass="comment",
+		commentThumbnailClass="comment-thumbnail",
+		commentContentClass="comment-content",
 		reactionClass="reaction",
 		inputClass="comment-input",
-	} = {}){
+	} = {},
+	{
+		dataExtractor,
+	}){
 		
-		this.objectives = [...document.querySelectorAll(target)];
+		this.commentsURI = commentsURI;
+		this.wsURI = wsURI;
+
+		this.dataExtractor = dataExtractor;
+
+		this.commentListClass = commentListClass;
+		this.commentContainerClass = commentContainerClass;
+		this.commentThumbnailClass = commentThumbnailClass;
+		this.commentContentClass = commentContentClass;
+		this.reactionClass = reactionClass;
+		this.inputClass = inputClass;
+
+		this.objective = document.querySelector(target);
+
+		this.fetchComments().then(comments => {
+			this.generateChannel(comments);												 
+		});
 
 	}
 
+	async fetchComments(){
+			return this.dataExtractor(await fetch(this.commentsURI));
+	}
+
 	generateComment(comment){
+		
+		const commentContainer = document.createElement(`<div class="${this.commentContainerClass}"></div>`);
+
+		const commentThumbnail = document.createElement(`<img src="${comment.thumbnail}" alt="thumbnail" class="${this.commentThumbnailClass}">`);
+
+		const commentContent = document.createElement(`<p class="${this.commentContentClass}"></p>`);
+		commentContet.textContent = comment.content;
+	
+		[commentThumbnail, commentContent].forEach(node => commentContainer.appendChild(node));
+
+		return commentContainer;
 
 	}
 
@@ -24,8 +62,10 @@ class Commentary {
 
 	generateChannel(comments){
 
+		
+
 	}
 
 }
 
-module.exports = Commentary;
+module.exports = Channel;
