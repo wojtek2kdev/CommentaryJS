3d19a62420a19591eb040ddc87f940181685d007
wojtek2kdev <wojtek2kdev@gmail.com>
2019-03-11 21:56:49 +0100

    Add vuex store for channel

--- client/preset.js
+++ client/preset.js
@@ -1,26 +1,32 @@
 const Vue = require('vue');
+const Vuex = require('vuex');
 
 class Preset {
 
-  constructor(component, ctx, {comments, userInfo, target, events}){
+  constructor(component, ctx){
    
     this.ctx = ctx;
     this.component = component;
-    this.events = events;
-    this.comments = comments;
-    this.userInfo = userInfo;
-    this.target = target;
+
+    Vue.use(Vuex);
+
+    this.store = new Vuex.Store({
+      state: {
+        comments: this.ctx.comments,
+        user: this.ctx.user,
+      },
+      actions: {
+        receivers: this.ctx.events.receivers,
+        senders: this.ctx.events.senders,
+      }
+    });
     
   }
 
   render(){
     this.instance = new Vue({
+      store: this.store,
       el: this.target,
-      data: {
-        comments,
-        userInfo,
-      },
-      methods: events,
       render: h => h(this.component)
     });
   }
