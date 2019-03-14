f440cfdd59314a91cb66112c73ec45b4c2cc9631
wojtek2kdev <wojtek2kdev@gmail.com>
2019-03-14 21:19:17 +0100

    Create file for channel component

---
+++ client/components/Channel.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="comment of comments" :key="comment">
+      <p>{{ comment.content }}</p>
+    </li>
+  </ul>
+</template>
+<script>
+  export default {
+    name: 'comments',
+    data: {
+      comments: ['comment1', 'Comment2'],
+    }
+  }
+</script>
+<style>
+
+</style>
