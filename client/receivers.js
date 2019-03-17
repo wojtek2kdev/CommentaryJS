7bd2564450af5429d5ca832976e531f4cd2648f6
wojtek2kdev <wojtek2kdev@gmail.com>
2019-03-17 23:07:31 +0100

    Add pipelines for modify appending comment

--- client/receivers.js
+++ client/receivers.js
@@ -1,7 +1,7 @@
 export const newComment = () => {
   return {
     name: 'appendComment',
-    action: (ctx, data) => ctx.commit('appendComment', data)
+    action: (ctx, data, ...pipelines) => ctx.commit('appendComment', pipelines.reduce((val, pipe) => val = pipe(val), data))
   }
 }
 
