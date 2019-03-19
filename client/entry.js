b8a33d50a1ffcd17d278520dc68c9f4cbbf7acb1
wojtek2kdev <wojtek2kdev@gmail.com>
2019-03-19 22:35:22 +0100

    Add channel component to entry

--- client/entry.js
+++ client/entry.js
@@ -1,7 +1,13 @@
 import { Channel } from './base';
 import Preset from './preset';
+import ChannelComponent from './components/Channel.vue';
+
+const components = {
+  ChannelComponent,
+};
 
 export {
   Channel,
-  Preset
+  Preset,
+  components,
 };
