e250fa1b1f56f53246050f36553c96a864d72192
wojtek2kdev <wojtek2kdev@gmail.com>
2019-03-19 22:35:05 +0100

    Change data to function returning obj

--- client/components/Channel.vue
+++ client/components/Channel.vue
@@ -1,15 +1,17 @@
 <template>
   <ul>
     <li v-for="comment of comments" :key="comment">
-      <p>{{ comment.content }}</p>
+      <p>{{ comment }}</p>
     </li>
   </ul>
 </template>
 <script>
   export default {
     name: 'comments',
-    data: {
-      comments: ['comment1', 'Comment2'],
+    data(){
+      return {
+        comments: [],
+      }
     }
   }
 </script>
