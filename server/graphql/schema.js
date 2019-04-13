c1517d438fb160bda9debce0c741c922f0b45af6
wojtek2kdev <wojtek2kdev@gmail.com>
2019-04-13 09:43:47 +0200

    Create graphql schema for querying

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -1,25 +1,79 @@
 import { gql } from 'apollo-server-express';
 
 import {
-  GraphQLTime,
+  GraphQLDateTime,
 } from 'graphql-iso-date';
 
-const schema =  gql`
+export default gql`
 
-  scalar Time
+  scalar DateTime, URL, EmojiCode 
+
+  type Comment {
+    _id: ID!
+    owner: User! 
+    content: String!
+    media: [URL]
+    reactions: [Reaction]
+    replies: [Reply]
+    channel_id: ID!
+    channel: Channel!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+
+  type Reply {
+    _id: ID!
+    owner: User!
+    content: String!
+    media: [URL]
+    reactions: [Reaction]
+    comment_id: ID!
+    comment: Comment
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+
+  type Reaction {
+    _id: ID!
+    owner: User!
+    to: ID!
+    emoji_id: ID!
+    emoji: Emoji 
+  }
+  
 
   type User {
     id: ID!
-    firstName: String
-    lastName: String
-    username: String!
-    thumbnail: String
-    profile: String
-    createdAt: Time!
+    name: String!
+    thumbnail: URL 
+    profile: URL 
+    createdAt: DateTime
+  }
+
+  type Channel {
+    _id: ID!
+    owner: User!
+    comments: [Comment]
+  }
+
+  type Emoji {
+    _id: ID!
+    unicode: String!
+    code: EmojiCode!
+  }
+
+  input Pagination {
+    from: Int = 0
+    to: Int = 10
+  }
+
+  schema {
+    Query {
+      channel(_id: ID!): Channel
+      comments(channelId: ID!, pagination: Pagination): [Comment]
+      replies(commentId: ID!, pagination: Pagination): [Reply]
+    }
   }
 
 `;
 
-Object.assign(schema._typeMap.Time, GraphQLTime);
-
-export default schema;
